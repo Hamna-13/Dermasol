8840864b0aaeee885edf0c509637cd15110260af
Hamna-13 <hamnahashmi09@gmail.com>
2025-11-28 17:49:08 +0500

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
-main
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from database import get_db
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI is running"}
+
+@app.get("/test-db")
+def test_db(db = Depends(get_db)):
+    result = db.execute(text("SELECT version();"))
+    return {"db_version": result.fetchone()}
