b49c9a85be33f189a7af1538c0fa984132791740
hamna-ali <hamnaali435@gmail.com>
2026-01-21 20:57:43 +0500

    frontend changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -89,5 +90,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 }
